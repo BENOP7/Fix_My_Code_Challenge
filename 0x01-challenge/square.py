96b005736aedbe5222fceaa52d138f3461abfd55
Benop7 <benjaminpetegnong@gmail.com>
2023-01-29 04:30:49 +0000

    first fix attempt

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -4,11 +4,14 @@
 """
 
 
-class Square:
-    '''
+class square:
+    """
         This class a model of a square with width and length
-    '''
+    """
     def __init__(self, *args, **kwargs):
+        """
+            Constructor
+        """
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -23,12 +26,12 @@ class Square:
         return (self.width + self.height) * 2
 
     def __str__(self):
+        """ String representation of object square """
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
-    s = Square(width=12, height=9)
+    s = square(width=12, height=9)
     print(s)
     print(s.area())
     print(s.perimeter())
